eaba826c6f0eb466d6f34ccd07ea2e306940e4dd
Gilberto Lopes <gilbertolif@dcc.ufrj.br>
2020-11-24 03:30:20 -0300

    + preparação para página de cadastro

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useForm, Controller } from 'react-hook-form';
+
+interface RegisterData {
+    name: string,
+    email: string,
+    phoneNumber: string,
+    password: string,
+}
 
 export default function Register() {
+    const { control, handleSubmit, errors } = useForm();
+    const onSubmit = (data: FormData) => { console.log(data) };
+    const navigation = useNavigation();
+
     return (
         <View style={styles.container}>
             <Text>Hello Register!</Text>
@@ -12,7 +25,29 @@ export default function Register() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
+        padding: 8,
         justifyContent: 'center',
+        backgroundColor: '#0e101c',
+    },
+    formBox: {
+        borderWidth: 1,
+        borderColor: 'black',
+        paddingVertical: 20,
+    },
+    label: {
+        margin: 10,
+        marginLeft: 0,
+        color: 'white',
+    },
+    button: {
+        marginTop: 20,
+        height: 40,
+        borderRadius: 4,
+    },
+    input: {
+        backgroundColor: 'white',
+        height: 40,
+        padding: 10,
+        borderRadius: 20,
     }
 });
